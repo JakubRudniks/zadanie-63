13cb84fb039bc5e1f9ba1c4961126404191d58dc
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2021-02-18 11:47:59 +0100

    Add files via upload

---
+++ zadanie 63.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+class file{
+
+    ifstream in;
+    int notowane = 0;
+    int size=0;
+    public:
+
+    void run();
+    bool dwucykliczny(string s);
+    bool d2jeden(string s);
+    
+};
+
+void file::run(){
+    in.open("ciagi.txt");
+    string oper;
+    for(int i =0 ; i<1000;++i){
+        in>>oper;
+        if(dwucykliczny(oper)){
+            cout <<oper<<endl;
+        }
+        if(d2jeden(oper)){
+            notwone++;
+        }
+    }
+    
+    cout<<notwone;
+    
+}
+
+bool file::dwucykliczny(string s){
+    string s1,s2;
+    if(s.size()%2==0){
+        if(s.substr(s.size()/2,s.size()/2)==s.substr(0,s.size()/2)){
+            return true;
+        }
+    }
+
+
+    return false;
+    
+}
+
+bool file::d2jeden(string s){
+
+    for(int i = 1;i<s.size();++i){
+        if(s[i]=='1' &&s[i-1]=='1'){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int main() {
+    file lup ;
+    lup.run();
+
+    return 0;
+    
+}
